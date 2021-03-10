7347d7dddc37d6cd031393073e99b2e5ffb5e5fd
Bry-inGH <80278816+Bry-inGH@users.noreply.github.com>
2021-03-10 10:38:17 +0000

    url in embedded.spec.js files changed / amended

--- cypress/integration/embedded.spec.js
+++ cypress/integration/embedded.spec.js
@@ -1,6 +1,6 @@
 describe('Embedded mode', () => {
   it('shows data', () => {
-    const url = 'https://github.com/Bry-inGH/family-tree-embedded/#' + Cypress.config('baseUrl');
+    const url = 'https://pewu.github.io/topola-embedded/#' + Cypress.config('baseUrl');
     cy.visit(url);
     cy.get('iframe')
     .its('0.contentDocument.body').should('not.be.empty')
